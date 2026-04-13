aa0f46078c2dc6214214845c4e6b368fff0f6faf
Muhammad Bima Ramadhan <34125207+bimambr@users.noreply.github.com>
2026-04-13 09:57:17 +0700

    flake: use upstream llama-cpp

--- flake.nix
+++ flake.nix
@@ -18,19 +18,9 @@
           config.allowUnfree = true;
         };
 
-        llamaCppCuda =
-          (pkgs.llama-cpp.override {
-            cudaSupport = true;
-          }).overrideAttrs (old: {
-            version = "8642"; # gemma4 support
-
-            src = pkgs.fetchFromGitHub {
-              owner = "ggml-org";
-              repo = "llama.cpp";
-              rev = "b8642";
-              hash = "sha256-CGoqQd4jdcVlttg1fFkUNW4v3Rxwfkj+TVlcHD59RhI=";
-            };
-          });
+        llamaCppCuda = pkgs.llama-cpp.override {
+          cudaSupport = true;
+        };
 
         pythonEnv = pkgs.python311.withPackages (ps:
           with ps; [
